2ac397e7fc047db9e4c4f76eabfcfd82ff506d96
Anders Aamodt <anders.aamodt@gmail.com>
2026-03-24 17:20:11 -0700

    Use Priorities trash icon in compose and increase size

--- static/blog-page.js
+++ static/blog-page.js
@@ -931,11 +931,7 @@
   }
 
   function composeTrashIconSvg() {
-    return '<svg class="trash-icon-svg" viewBox="0 0 24 24" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">' +
-      '<path d="M9 3.75h6a1 1 0 0 1 1 1V6h4v1.75H4V6h4V4.75a1 1 0 0 1 1-1Z" fill="currentColor"/>' +
-      '<path d="M6.75 8.5h10.5l-.7 10.02a2 2 0 0 1-2 1.86H9.45a2 2 0 0 1-2-1.86L6.75 8.5Z" fill="currentColor"/>' +
-      '<path d="M10.2 10.75v6.7M13.8 10.75v6.7" stroke="var(--bg, #fff)" stroke-width="1.4" stroke-linecap="round"/>' +
-      '</svg>';
+    return '<svg class="trash-icon-svg" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 7h16m-10 4v6m4-6v6M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3"/></svg>';
   }
 
   function readComposeFields() {
