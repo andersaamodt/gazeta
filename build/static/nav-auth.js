0285f6a8a0b71c668bd4f993fd6d160e1ccef0b9
Anders Aamodt <anders.aamodt@gmail.com>
2026-05-23 02:13:03 -0700

    Center nav panel and mask title actions

--- build/static/nav-auth.js
+++ build/static/nav-auth.js
@@ -3783,7 +3783,7 @@
 
   function updateThemeStylesheet(theme) {
     var nextTheme = normalizeThemeName(theme);
-    var href = '/static/themes/' + encodeURIComponent(nextTheme) + '.css?v=20260523-contact-nav1';
+    var href = '/static/themes/' + encodeURIComponent(nextTheme) + '.css?v=20260523-contact-nav2';
     var themeLink = document.getElementById('theme-stylesheet');
     if (isThemeHrefAlreadyActive(themeLink, href)) {
       return Promise.resolve();
